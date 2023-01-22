4caf2476654643c746560c4237d7d1620d0c0e50
sebastian24091998 <mollosebastian4@gmail.com>
2023-01-22 02:42:33 -0400

    cambios7

--- resources/views/layouts/theme/styles.blade.php
+++ resources/views/layouts/theme/styles.blade.php
@@ -1,9 +1,10 @@
-<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/plugins.css" rel="stylesheet" type="text/css" />
-    <link href="assets/css/structure.css" rel="stylesheet" type="text/css" class="structure" />
-    <!-- END GLOBAL MANDATORY STYLES -->
+<script src="{{ asset ('assets/js/loader.js') }}"></script>
+<link href="{{ asset ('assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
 
-    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
-    <link href="plugins/apex/apexcharts.css" rel="stylesheet" type="text/css">
-    <link href="assets/css/dashboard/dash_2.css" rel="stylesheet" type="text/css" class="dashboard-sales" />
+<link href="https://fonts.googleapis.com/css?family=Quicksand:400,500,600,700&display=swap" rel="stylesheet">
+<link href="{{ asset ('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset ('assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset ('assets/css/structure.css') }}" rel="stylesheet" type="text/css" class="structure" />
+
+<link href="{{ asset ('plugins/apex/apexcharts.css') }}" rel="stylesheet" type="text/css">
+<link href="{{ asset ('assets/css/dashboard/dash_2.css') }}" rel="stylesheet" type="text/css" class="dashboard-sales" />
