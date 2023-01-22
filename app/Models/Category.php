fbe694a051e0913dc7b276c94ce1aa98045b157f
sebastian24091998 <mollosebastian4@gmail.com>
2023-01-21 23:03:05 -0400

    cambio4

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,8 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Company extends Model
+class Category extends Model
 {
     use HasFactory;
-    protected $fillable =['name','adress','phone','taxpayer_id'];
+    protected $fillable = ['name','image'];
 }
